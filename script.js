0e118750b9fe16c38a7d06bdc094f305bc8fcbba
Nicola Paoletti <nicola97pao@hotmail.it>
2021-12-13 21:54:17 +0100

    XD

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const img = new Image();
-img.src = "./media/flappy-bird-set.png";
+img.src = "./media/flappy-bird-set2.png";
 
 // general settingssss
 let gamePlaying = false;
